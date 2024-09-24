93b74a6ca3a5dc0f2f179c30da97f4d0db478fad
Arunava-Barua <rahulbarua31@gmail.com>
2024-09-24 23:26:29 +0530

    Added CTA to approvals command

--- push-showrunners-framework/src/showrunners/walletTracker/walletTrackerRoutes.ts
+++ push-showrunners-framework/src/showrunners/walletTracker/walletTrackerRoutes.ts
@@ -12,46 +12,6 @@ export default (app: Router) => {
   // routes for wallet tracker
 
   route.post(
-    '/load_tokens',
-    celebrate({
-      body: Joi.object({
-        simulate: [Joi.bool(), Joi.object()],
-      }),
-    }),
-    middlewares.onlyLocalhost,
-    async (req: Request, res: Response, next: NextFunction) => {
-      const Logger: any = Container.get('logger');
-      Logger.debug('Calling /showrunners/load_tokens endpoint with body: %o', req.body.simulate.logicOverride.addressesWithPositions[0]);
-      
-        const wt = Container.get(WallettrackerChannel);
-        const cycleValue = await globalCycleModel.findOne({_id: "global"})
-    ||
-   (await globalCycleModel.create({
-      _id: "global",
-     lastCycle: 1
-   }));
-   const channel = Container.get(WallettrackerChannel);
-
-    Logger.info(`Cycle Value ${cycleValue}`);
-   
-      if(cycleValue.lastCycle == 1){
-      channel.fetchBalance();
-     Logger.info("First case");
-     await globalCycleModel.updateOne({_id:'global'},{ $inc: {lastCycle: 1 } }); 
-    }else{
-      Logger.info("Second case");
-      channel.fetchPortolio()
-      channel.fetchBalance();
-    await globalCycleModel.updateOne({_id:'global'},{ $inc: {lastCycle: -1 } }); 
-      } 
-      return res.status(201).json({ success: true, data: "GM" });
-
-
-
-}
-  );
-
-  route.post(
     '/apr',
     celebrate({
       body: Joi.object({
@@ -64,8 +24,15 @@ export default (app: Router) => {
       Logger.debug('Calling /showrunners/load_tokens endpoint with body: %o', req.body);
       try {
         const wt = Container.get(WallettrackerChannel);
-        const response = await wt.calculateWalletPerformance(1000,'0xaB8a67743325347Aa53bCC66850f8F13df87e3AF','https://app.push.org/yieldv2',true,1000,1);
-        
+        const response = await wt.calculateWalletPerformance(
+          1000,
+          '0xaB8a67743325347Aa53bCC66850f8F13df87e3AF',
+          'https://app.push.org/yieldv2',
+          true,
+          1000,
+          1,
+        );
+
         return res.status(201).json({ success: true, data: response });
       } catch (e) {
         Logger.error('🔥 error: %o', e);
@@ -89,7 +56,7 @@ export default (app: Router) => {
       try {
         const wt = Container.get(WallettrackerChannel);
         const response = await wt.sendEventInfo();
-        
+
         return res.status(201).json({ success: true, data: response });
       } catch (e) {
         Logger.error('🔥 error: %o', e);
@@ -98,27 +65,99 @@ export default (app: Router) => {
     },
   );
 
-    //route for Sending Transaction History
-    route.post(
-      '/getHistory',
-      celebrate({
-        body: Joi.object({
-          simulate: [Joi.bool(), Joi.object()],
-        }),
+  //route for Sending Transaction History
+  route.post(
+    '/getHistory',
+    celebrate({
+      body: Joi.object({
+        simulate: [Joi.bool(), Joi.object()],
       }),
-      middlewares.onlyLocalhost,
-      async (req: Request, res: Response, next: NextFunction) => {
-        const Logger: any = Container.get('logger');
-        Logger.debug('Calling /showrunners/getHistory endpoint with body: %o', req.body);
-        try {
-          const wt = Container.get(WallettrackerChannel);
-          const response = await wt.sendWalletTransactions();
-          
-          return res.status(201).json({ success: true, data: response });
-        } catch (e) {
-          Logger.error('🔥 error: %o', e);
-          return next(e);
-        }
-      },
-    );
+    }),
+    middlewares.onlyLocalhost,
+    async (req: Request, res: Response, next: NextFunction) => {
+      const Logger: any = Container.get('logger');
+      Logger.debug('Calling /showrunners/getHistory endpoint with body: %o', req.body);
+      try {
+        const wt = Container.get(WallettrackerChannel);
+        const response = await wt.sendWalletTransactions();
+
+        return res.status(201).json({ success: true, data: response });
+      } catch (e) {
+        Logger.error('🔥 error: %o', e);
+        return next(e);
+      }
+    },
+  );
+
+  //route for sending hack report notifications
+  route.post(
+    '/hack_notif',
+    celebrate({
+      body: Joi.object({
+        simulate: [Joi.bool(), Joi.object()],
+      }),
+    }),
+    middlewares.onlyLocalhost,
+    async (req: Request, res: Response, next: NextFunction) => {
+      const logger: any = Container.get('logger');
+      logger.debug('Calling /showrunners/wt ticker endpoint with body: %o', req.body);
+      try {
+        const hackerNews = Container.get(WallettrackerChannel);
+        const response = await hackerNews.checkNewHacks();
+
+        return res.status(201).json({ success: true, data: response });
+      } catch (e) {
+        logger.error('🔥 error: %o', e);
+        return next(e);
+      }
+    },
+  );
+
+  //route for sending yield opportunity notifications
+  route.post(
+    '/yield_notif',
+    celebrate({
+      body: Joi.object({
+        simulate: [Joi.bool(), Joi.object()],
+      }),
+    }),
+    middlewares.onlyLocalhost,
+    async (req: Request, res: Response, next: NextFunction) => {
+      const logger: any = Container.get('logger');
+      logger.debug('Calling /showrunners/wt ticker endpoint with body: %o', req.body);
+      try {
+        const hackerNews = Container.get(WallettrackerChannel);
+        const response = await hackerNews.checkNewYieldOpportunities();
+
+        return res.status(201).json({ success: true, data: response });
+      } catch (e) {
+        logger.error('🔥 error: %o', e);
+        return next(e);
+      }
+    },
+  );
+
+  // route for sending token approvals
+  route.post(
+    '/approvals',
+    celebrate({
+      body: Joi.object({
+        simulate: [Joi.bool(), Joi.object()],
+      }),
+    }),
+    middlewares.onlyLocalhost,
+    async (req: Request, res: Response, next: NextFunction) => {
+      const logger: any = Container.get('logger');
+      logger.debug('Calling /showrunners/wt ticker endpoint with body: %o', req.body);
+      try {
+        const hackerNews = Container.get(WallettrackerChannel);
+        const response = await hackerNews.checkApprovals();
+
+        return res.status(201).json({ success: true, data: response });
+      } catch (e) {
+        logger.error('🔥 error: %o', e);
+        return next(e);
+      }
+    },
+  );
 };
